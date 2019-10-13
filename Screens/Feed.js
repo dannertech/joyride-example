d3bf31c8f45e7afd7c39408bcf425e4e2745624f
dannertech <danner@danner.tech>
2019-10-13 06:36:08 -0400

    built out Home and Feed pages

---
+++ Screens/Feed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text} from 'react-native';
+
+export default class Feed extends Component {
+    render(){
+        return(
+            <View>
+                <Text>This is the Feed page</Text>
+            </View>
+        )
+    }
+}
---
+++ Screens/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default class Home extends Component {
+    render(){
+        return(
+            <View>
+                <Text>
+                    This is the Home page
+                </Text>
+            </View>
+        )
+    }
+}
+
