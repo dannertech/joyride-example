3217ee3997f98aa29d1978090e3eba47e4d5edb7
dannertech <danner@danner.tech>
2019-10-15 20:36:24 -0400

    added custom buttons

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Image } from 'react-native';
 
-import { Card, CardItem, View, Body, Button, Left, Right, Text, Container, Content } from 'native-base';
+import { Icon, Card, CardItem, View, Body, Button, Left, Right, Text, Container, Content } from 'native-base';
 const firstWine = require('../Images/first-wine.png');
 const secondWine = require('../Images/second-wine.png');
+const thirdWine = require('../Images/third-wine.png');
 const profilePicture = require('../Images/profile-pic.png');
+const addButton = require('../Images/add-button.png');
+const cartButton = require('../Images/cart-button.png');
+
 
 
 export default class Home extends Component {
@@ -16,10 +20,54 @@ export default class Home extends Component {
                         <CardItem cardBody>
                             <Image source={secondWine} style={{height: 200, width: null, flex: 1}}/>
                         </CardItem>
+                        <CardItem>
+                            <Text style={{fontStyle: 'italic'}}>Muller</Text>
+                        </CardItem>
+                        <CardItem>
+                            <Left>
+                                <Button transparent>
+                                    <Image source={addButton} />
+                                </Button>
+                                <Button transparent>
+                                    <Image source={cartButton}/>
+                                </Button>
+                            </Left>
+                        </CardItem>
+                    </Card>
+                    <Card>
+                        <CardItem cardbody>
+                            <Image source={thirdWine} style={{height: 105, width: null, flex: 1}}/>    
+                        </CardItem>
+                        <CardItem>
+                            <Text style={{fontStyle: 'italic'}}>French and Bubbly</Text>
+                        </CardItem>
+                        <CardItem>
+                            <Left>
+                                <Button transparent>
+                                    <Image source={addButton} />
+                                </Button>
+                                <Button transparent>
+                                    <Image source={cartButton}/>
+                                </Button>
+                            </Left>
+                        </CardItem>
                     </Card>
                     <Card>
                         <CardItem cardBody>
-                            <Image source={firstWine} style={{height: 150, width: null, flex: 1}}/>
+                            <Image source={firstWine} style={{height: 125, width: null, flex: 1}}/>
+                        </CardItem>
+                        <CardItem>
+                            <Text style={{fontStyle: 'italic'}}>First Pois</Text>
+                        </CardItem>
+                        <CardItem>
+                            <Left>
+                                <Button transparent>
+                                    <Image source={addButton} />
+                                </Button>
+                                <Button transparent>
+                                    <Image source={cartButton}/>
+                                </Button>
+                            </Left>
                         </CardItem>
                     </Card>
                 </Content>
